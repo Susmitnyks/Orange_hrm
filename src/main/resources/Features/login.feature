8f4aef76e2ab9698026cc5f91671887ea9b06c95
SusmitSurwade <susmit.s.surwade@gmail.com>
2024-01-04 11:39:07 +0530

    Login page

---
+++ src/main/resources/Features/login.feature
@@ -0,0 +1,7 @@
+Feature: Test login functionality
+
+  @Login_test_case
+   Scenario: Verify successful login with valid credentials
+    Given User is on the login page
+#    When User enters username and password
+#    Then User is navigated to home page
---
+++ src/test/java/runner/MyTestRunner.java
@@ -0,0 +1,18 @@
+package runner;
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+@RunWith(Cucumber.class)
+@CucumberOptions(features =
+        {
+                "src/main/resources/Features",
+        },
+        glue = { "runner"}, monochrome = true, tags = ("@Login_test_case"), dryRun = false, plugin = {
+        "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+        "timeline:test-output-thread/" }
+)
+public class MyTestRunner{
+
+}
